e868081a847925f43a04135cfeddf2abab3ce84a
AdityaChaudhary3 <adityac2356@gmail.com>
2023-10-16 19:50:30 +0530

    setup react-router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+
 
 function App() {
 
@@ -10,7 +12,8 @@ function App() {
   return (
     <>
       <Header />
-      <h1 className='bg-red-500'>Aditya</h1>
+      {/* <h1 className='bg-red-500'>Aditya</h1> */}
+        <Outlet />
       <Footer />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Home</h1>,
+      },
+      {
+        path: "/projects",
+        element: <h1>Projects</h1>,
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1>,
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
